9979ee47e97bbcc06ea5209e8ec8cd02c1c5e466
Itai Sasson <sassonitai1@gmail.com>
2019-09-29 15:59:12 +0300

    Basic Manual Landmarker

---
+++ alpha/preprocessing/manual_landmarker.py
@@ -0,0 +1,35 @@
+import cv2
+import json
+import os
+
+
+# iterate over each image in data_raw directory
+for img_name in os.listdir('data_raw'):
+
+	landmarks = []
+
+	# draw and add landmark to list
+	def draw_circle(event, x, y, flags, param):
+		global landmarks
+		if event == cv2.EVENT_LBUTTONDBLCLK:
+			cv2.circle(img, (x, y), 3, (255, 0, 0), -1)
+			landmarks.append((x, y))
+			print(x, y)
+
+	# open image
+	img = cv2.imread('data_raw/' + img_name, 10)
+	cv2.imshow('image', img)
+	cv2.namedWindow('image')
+	cv2.setMouseCallback('image', draw_circle)
+
+	# save landmark positions to unique file in data_marked directory
+	while True:
+		cv2.imshow('image', img)
+		if cv2.waitKey(1) == ord('q'):
+			# add landmarks to JSON file for specific image if landmarks were added
+			if landmarks != []:
+				with open('data_marked/' + img_name + '.json', 'w', encoding='utf-8') as f:
+							json.dump({img_name: landmarks}, f, ensure_ascii=False, indent=4)
+			break
+
+	cv2.destroyAllWindows()
