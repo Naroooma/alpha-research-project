d6c3c9f2ccbac3f3803319b55dd59003cc366e57
Itai Sasson <sassonitai1@gmail.com>
2019-10-05 21:20:03 +0300

    loss implementation

--- alpha/image/landmarker/loss.py
+++ alpha/image/landmarker/loss.py
@@ -1,17 +1,31 @@
-import cv2
 import json
 import os
 from pathlib import Path
+import numpy as np
 
 
-# open json files
-json_folder = Path(os.path.dirname(os.getcwd())) / "preprocessing/data_marked"
+def image_L2(img, results_folder, truth_folder):
+	# results JSON
+	with open(results_folder / img) as f:
+		results_list = np.array(json.load(f))[0]
 
-for json_file in os.listdir(json_folder):
-	with open(json_folder / json_file) as f:
-		json_info = json.load(f)
-	# print landmarks for each image
-	for landmarks_list in json_info.values():
-		print(json_file)
-		for landmark in landmarks_list:
-			print(landmark)
+	# truth JSON
+	with open(truth_folder / img) as f:
+		truth_list = np.array(json.load(f))[0]
+
+	# loss calculation
+	loss = np.dot(results_list - truth_list, results_list - truth_list)
+	return loss
+
+
+truth_folder = Path(os.path.dirname(os.getcwd())) / "preprocessing/truth"
+results_folder = Path('results')
+total_loss = 0
+
+# iterate over each image in results_folder
+for results_file in os.listdir(results_folder):
+	print(results_file)
+	total_loss += image_L2(results_file, results_folder, truth_folder)
+
+# print average loss
+print(total_loss / len(os.listdir(results_folder)))
